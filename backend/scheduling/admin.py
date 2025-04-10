15dec50499275e2b87643b1c02ad0f2b30e0cb80
Jerome <donfacjc@dukes.jmu.edu>
2025-04-10 19:17:01 -0400

    added basic models, as well as permanent TA, Student, and Educator groups

--- backend/scheduling/admin.py
+++ backend/scheduling/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User, Question, Schedule, Subject, School
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Schedule)
+admin.site.register(Subject)
+admin.site.register(School)
