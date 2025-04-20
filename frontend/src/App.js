fe972a92f84fa02bdae5a2cf076f2bda9079825d
Jerome <donfacjc@dukes.jmu.edu>
2025-04-20 17:05:32 -0400

    properly style the schedule page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,17 @@ import { AuthContextProvider } from "./auth";
 import Router from "./Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "./components/ui/provider";
-import './css/global.css'
+// import "./css/global.css";
+import "@syncfusion/ej2-base/styles/material.css";
+import "@syncfusion/ej2-buttons/styles/material.css";
+import "@syncfusion/ej2-calendars/styles/material.css";
+import "@syncfusion/ej2-dropdowns/styles/material.css";
+import "@syncfusion/ej2-inputs/styles/material.css";
+import "@syncfusion/ej2-navigations/styles/material.css";
+import "@syncfusion/ej2-popups/styles/material.css";
+import "@syncfusion/ej2-splitbuttons/styles/material.css";
+import "@syncfusion/ej2-react-grids/styles/material.css";
+import "@syncfusion/ej2-react-schedule/styles/material.css";
 
 function App() {
   const queryClient = new QueryClient();
