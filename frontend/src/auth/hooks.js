a9d96873c4f5088630b2ac7b5747a7cd8bdac9d7
Noodlesv21 <royji@dukes.jmu.edu>
2025-04-25 23:55:49 -0400

    Don't know how i deleted that by accident sorry

--- frontend/src/auth/hooks.js
+++ frontend/src/auth/hooks.js
@@ -20,7 +20,7 @@ export function useAuthInfo () {
 }
 
 function authInfo (auth) {
-  const isAuthenticat = auth.status === 200 || (auth.status === 401 && auth.meta.is_authenticated)
+  const isAuthenticated = auth.status === 200 || (auth.status === 401 && auth.meta.is_authenticated)
   const requiresReauthentication = isAuthenticated && auth.status === 401
   const pendingFlow = auth.data?.flows?.find(flow => flow.is_pending)
   return { isAuthenticated, requiresReauthentication, user: isAuthenticated ? auth.data.user : null, pendingFlow }
